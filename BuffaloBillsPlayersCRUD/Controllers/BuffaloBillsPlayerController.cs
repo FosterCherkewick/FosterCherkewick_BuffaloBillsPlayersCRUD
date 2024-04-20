c66cd7f3d189f01e2cbaafdd0fbf4db686d90201
FosterCherkewick <112504701+FosterCherkewick@users.noreply.github.com>
2024-04-19 18:17:18 -0600

    added project and first .cs file

---
+++ BuffaloBillsPlayersCRUD/Controllers/BuffaloBillsPlayerController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuffaloBillsPlayersCRUD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BuffaloBillsPlayerController : ControllerBase
+    {
+    }
+}
---
+++ BuffaloBillsPlayersCRUD/Models/BuffaloBillsPlayer.cs
@@ -0,0 +1,11 @@
+﻿//BuffaloBillsPlayer.cs
+namespace BuffaloBillsPlayersCRUD.Models
+{
+    public class BuffaloBillsPlayer
+    {
+        public int Number {  get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; } = string.Empty;
+        public bool isActive { get; set; } = true;
+    }
+}
